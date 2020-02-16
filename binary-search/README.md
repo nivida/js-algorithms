c29d105ca82131941da048fd592a5765fb3371d2
nivida <samuel@ethereum.org>
2020-02-16 12:58:14 +0100

    funcDocs and example added for binary-search

--- binary-search/README.md
+++ binary-search/README.md
@@ -7,3 +7,12 @@ Searching of a sorted array.
 3. If the value of the search key is higher than the item in the midlle iterate through the upper half.
 
 TC: ``O(log n)``
+
+## Example
+
+``` javascript
+import {binarySearch} from './index.js';
+
+const searchArray = [0, 1, 5, 10, 20, 25, 30, 40, 42, 50];
+console.log(binarySearch('asc', searchArray, 0, searchArray.length, 30);
+```
--- binary-search/index.js
+++ binary-search/index.js
@@ -1,4 +1,15 @@
 
+/**
+ * Returns the index of the searched element.
+ * 
+ * @param {string} sortOrder - 'asc' or 'desc'
+ * @param {Array<number>} sortedArray 
+ * @param {number} start 
+ * @param {number} end 
+ * @param {number} searchedValue 
+ * 
+ * @returns {number} - The index of the element
+ */
 export function binarySearch(sortOrder, sortedArray, start, end, searchedValue) {
     if (end >= 1) {
         const middle = getMiddle(start, end)
@@ -17,6 +28,15 @@ export function binarySearch(sortOrder, sortedArray, start, end, searchedValue) 
     }
 }
 
+/**
+ * Returns true if the searched value is in the left half of the array.
+ * 
+ * @param {string} sortOrder - 'asc' or 'desc'
+ * @param {number} element 
+ * @param {number} searchedValue
+ * 
+ * @returns {boolean} 
+ */
 function searchLeftHalf(sortOrder, element, searchedValue) {
     if (sortOrder === 'asc') {
         return element < searchedValue;
@@ -25,6 +45,15 @@ function searchLeftHalf(sortOrder, element, searchedValue) {
     return element > searchedValue;
 }
 
+/**
+ * Returns true if the searched value in the right half of the array
+ * 
+ * @param {string} sortOrder - 'asc' or 'desc'
+ * @param {number} element 
+ * @param {number} searchedValue 
+ * 
+ * @returns {boolean}
+ */
 function searchRightHalf(sortOrder, element, searchedValue) {
     if (sortOrder === 'asc') {
         return element > searchedValue;
@@ -33,6 +62,14 @@ function searchRightHalf(sortOrder, element, searchedValue) {
     return element < searchedValue;
 }
 
+/**
+ * Returns the middle index
+ * 
+ * @param {number} start 
+ * @param {number} end
+ * 
+ * @returns {number} 
+ */
 function getMiddle(start, end) {
     return Math.floor(start + (end - 1) / 2);
 }
