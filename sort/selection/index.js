7ec54b4c4c2b69fff2a8e5d7caed2c64b8125fa0
nivida <samuel@ethereum.org>
2020-02-16 21:46:34 +0100

    explanatory comments added to selection sort

--- sort/selection/index.js
+++ sort/selection/index.js
@@ -11,15 +11,19 @@ export function selectionSort(sortOrder, array) {
     const length = array.length - 1;
     let lowestItem = 0;
 
+    // Iterate over each item of the array
     for (let i = 0; i <= length; i++) {
         lowestItem = i;
 
+        // Iterate over each sub array item and detect the lowest value
         for (let y = i + 1; y < length; y++) {
+            // Compare item one with item two depending on the configured sort order
             if (compare(sortOrder, array[y], array[lowestItem])) {
                 lowestItem = y;
             }
         }
 
+        // Move lowest element to the first position of the array
         array = swap(lowestItem, i);
     }
 
